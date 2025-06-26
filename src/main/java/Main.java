3e238349f6984b758f4e24ae7c74cc02481f4b5b
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-26 12:41:58 -0300

    Java - Operadores aritmeticos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,21 @@
 
 public class Main {
     public static void main(String[] args) {
-        int idade = 25;
-        long quantidadeChachorros = 5L;
-        double salarioMensal = 5000.00;
-        char genero = 'M';
-        boolean empregado = true;
-        float peso = 79.05f;
+        double pao = 9.50;
+        double queijo = 7.00;
+        double acucar = 1.00;
+        double desconto = 5.00;
+        int diasDoMes = 30;
 
-        System.out.println("Idade:              " + idade);
-        System.out.println("Quantidade de cães: " + quantidadeChachorros);
-        System.out.println("Salario Mensal:     " + salarioMensal);
-        System.out.println("Gênero:             " + genero);
-        System.out.println("Empregado:          " + empregado);
-        System.out.println("Peso:               " + peso);
+        double valorTotal = pao + queijo + acucar;
+        double valorTotalComDesconto = valorTotal - desconto;
+        double valorTotalDividido = valorTotalComDesconto / 2;
+        double valorTotalMensal = valorTotalComDesconto * diasDoMes;
+
+        System.out.println("valorTotal:              " + valorTotal);
+        System.out.println("valorTotalComDesconto:   " + valorTotalComDesconto);
+        System.out.println("valorTotalDividido:      " + valorTotalDividido);
+        System.out.println("valorTotalMensal:        " + valorTotalMensal);
+
     }
 }
