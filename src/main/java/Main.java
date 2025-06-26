6b7b47a71d9b6ff3f55c2ed286fc913aa3a0351c
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-26 12:57:03 -0300

    Java - Trabalhando com Strings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        double pao = 9.50;
-        double queijo = 7.00;
-        double acucar = 1.00;
-        double desconto = 5.00;
-        int diasDoMes = 30;
+        String nome = "Joana";
+        String sobrenome = "da Silva";
 
-        double valorTotal = pao + queijo + acucar;
-        double valorTotalComDesconto = valorTotal - desconto;
-        double valorTotalDividido = valorTotalComDesconto / 2;
-        double valorTotalMensal = valorTotalComDesconto * diasDoMes;
+        int tamanhoString = nome.length();
+        boolean saoIguais = nome.equals(sobrenome);
+        String nomeCompleto = nome.concat(sobrenome);
 
-        System.out.println("valorTotal:              " + valorTotal);
-        System.out.println("valorTotalComDesconto:   " + valorTotalComDesconto);
-        System.out.println("valorTotalDividido:      " + valorTotalDividido);
-        System.out.println("valorTotalMensal:        " + valorTotalMensal);
+        System.out.println("Olá " + nome.toLowerCase() + " seu nome tem " + tamanhoString + " caracteres.");
+        System.out.println("O nome e sobrenome são iguais? " + saoIguais);
+        System.out.println("Nome Completo: " + nomeCompleto);
 
     }
 }
