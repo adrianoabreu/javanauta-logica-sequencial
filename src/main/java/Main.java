38097176f2e740737dde30233058f62051250e62
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-26 11:50:03 -0300

    Java - Logica sequencial

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Instrução numero 1");
+        System.out.println("Instrução numero 2");
+        System.out.println("Instrução numero 3");
+        System.out.println("Instrução numero 4");
+    }
+}
