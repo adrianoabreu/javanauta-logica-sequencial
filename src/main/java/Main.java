b61515ba18b2c90d3b15982df3686c3cbc701b63
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-26 20:09:14 -0300

    Java - Trabalhando com Arrays

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        for(int cont = 0; cont < 5; cont++){
-            System.out.println("Contador: " + cont);
+        int[] numeros = {10,20,30,50,1};
+        String[] frutas;
+
+        frutas = new String[]{"Maçã", "Morango", "Abacaxi"};
+
+        double[] salarios = new double[5];
+        salarios[3] = 500.00;
+        salarios[1] = 200.00;
+        salarios[0] = 150.00;
+        salarios[2] = 25.00;
+        salarios[4] = 100.00;
+
+        //iteração de array com for
+        for(int posicao = 0; posicao < salarios.length; posicao++){
+            System.out.println("Salario: " + salarios[posicao]);
+        }
+
+        //foreach
+        for(double salario : salarios){
+            System.out.println("Salario: " + salario);
         }
 
     }
