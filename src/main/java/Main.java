b2b8af84ed07e0c42b87c64b7299c5792692736e
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-26 12:24:19 -0300

    Java - Variaveis e tipos primitivos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Instrução numero 1");
-        System.out.println("Instrução numero 2");
-        System.out.println("Instrução numero 3");
-        System.out.println("Instrução numero 4");
+        int idade = 25;
+        long quantidadeChachorros = 5L;
+        double salarioMensal = 5000.00;
+        char genero = 'M';
+        boolean empregado = true;
+        float peso = 79.05f;
+
+        System.out.println("Idade:              " + idade);
+        System.out.println("Quantidade de cães: " + quantidadeChachorros);
+        System.out.println("Salario Mensal:     " + salarioMensal);
+        System.out.println("Gênero:             " + genero);
+        System.out.println("Empregado:          " + empregado);
+        System.out.println("Peso:               " + peso);
     }
 }
