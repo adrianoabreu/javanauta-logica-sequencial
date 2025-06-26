d2b2a00dddb3d01e580df9de193dbecfa7954f18
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-26 15:49:25 -0300

    Java - Expressoes comparativas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int numero = 10;
-        double d = numero;
+        int pao = 10;
+        int acucar = 20;
 
-        double d2 = 10.5;
-        int int2 = (int) d2;
-
-        Integer numeroInteger = 10;
-        String numeroString = numeroInteger.toString();
-
-        Double numeroDouble = 10.0;
-        String numeroDoubleString = numeroDouble.toString();
-
-        String numeroStringDois = "1234";
-        Integer numeroConvertido = Integer.parseInt(numeroStringDois);
-        Double numeroConvertidoDouble = Double.parseDouble(numeroStringDois);
-        Long numeroConvertidoLong = Long.parseLong(numeroStringDois);
-
-        Long numeroLong = 10L;
-        String numeroStringLong = numeroLong.toString();
-
-        System.out.println(d);
-        System.out.println(int2);
-        System.out.println(numeroInteger + " " + numeroString);
-        System.out.println(numeroStringDois + " " + numeroConvertido);
+        System.out.println(pao == acucar);
+        System.out.println(pao != acucar);
+        System.out.println(pao > acucar);
+        System.out.println(pao < acucar);
+        System.out.println(pao >= acucar);
+        System.out.println(pao <= acucar);
     }
 }
