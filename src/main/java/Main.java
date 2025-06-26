22e97a284155014d66d213af4de530a242be17ea
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-26 20:39:08 -0300

    Java - Trabalhando com Collections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] numeros = {10,20,30,50,1};
-        String[] frutas;
+        List<String> listaDeCompras = new ArrayList<>();
+        listaDeCompras.add("Mamão");
+        listaDeCompras.add("Sabão em pó");
+        listaDeCompras.add("Macarrão");
+        listaDeCompras.add(1,"Banana");
 
-        frutas = new String[]{"Maçã", "Morango", "Abacaxi"};
-
-        double[] salarios = new double[5];
-        salarios[3] = 500.00;
-        salarios[1] = 200.00;
-        salarios[0] = 150.00;
-        salarios[2] = 25.00;
-        salarios[4] = 100.00;
-
-        //iteração de array com for
-        for(int posicao = 0; posicao < salarios.length; posicao++){
-            System.out.println("Salario: " + salarios[posicao]);
+        for (String item : listaDeCompras) {
+            System.out.println(item);
         }
 
-        //foreach
-        for(double salario : salarios){
-            System.out.println("Salario: " + salario);
+        String item = listaDeCompras.get(3);
+        System.out.println("Na posição 3 eu tenho: " + item);
+
+        listaDeCompras.remove("Sabão em pó");
+
+        for(String item2 : listaDeCompras){
+            System.out.println(item2);
         }
+
+        boolean eVazio = listaDeCompras.isEmpty();
+        int tamanho = listaDeCompras.size();
+        boolean contem = listaDeCompras.contains("Arroz");
+
+        System.out.println("Lista vazia? " + eVazio);
+        System.out.println("Tamanho da lista: " + tamanho);
+        System.out.println("Lista contem o item Arroz? " + contem);
+
+        listaDeCompras.forEach(produto -> System.out.println("Produtos: " + produto));
+
+        listaDeCompras.clear();
+        System.out.println("A lista está vazia? " + listaDeCompras.isEmpty());
+
+
 
     }
 
