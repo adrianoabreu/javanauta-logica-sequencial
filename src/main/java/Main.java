9e1b1f5bb7960c7c941c0ac85f3d808a9cdda75d
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-26 19:04:15 -0300

    Java - Estrutura repetitiva While

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int diaDaSemana = 5;
+        int contador = 0;
 
-        switch (diaDaSemana) {
-            case 1:
-                System.out.println("Domingo");
-                break;
-            case 2:
-                System.out.println("Segunda");
-                break;
-            case 3:
-                System.out.println("Terça");
-                break;
-            case 4:
-                System.out.println("Quarta");
-                break;
-            case 5:
-                System.out.println("Quinta");
-                break;
-            case 6:
-                System.out.println("Sexta");
-                break;
-            case 7:
-                System.out.println("Sábado");
-                break;
+        while(contador < 5) {
+            System.out.println("Contador: " + contador);
+            contador++;
         }
     }
 
